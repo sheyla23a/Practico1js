e929b825838b16237f26c2ba03212574b2b178eb
sheyla astorga <sheylaastorga1998@gmail.com>
2023-10-26 17:41:51 -0300

    agregue y desarrolle consigna del ejercicio4 del TP1js

---
+++ js/TP1ej4.js
@@ -0,0 +1,8 @@
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+// Ejemplo:
+
+// input: Coder
+// Output: Hola Coder
+
+const nombreUsuario = prompt("Por favor, ingresa tu nombre:");
+console.log("Hola" +" "+ nombreUsuario);
