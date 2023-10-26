fbfb4354be6e2ee1ab6f26015d41f2c39b55488c
sheyla astorga <sheylaastorga1998@gmail.com>
2023-10-26 17:42:59 -0300

    agregue y desarrolle consigna del ejercicio5 del TP1js

---
+++ js/TP1ej5.js
@@ -0,0 +1,13 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// Nota: Tener en cuenta la siguiente función: parseInt
+
+let num1 = parseInt(prompt("Ingrese el primer número:"));
+let num2 = parseInt(prompt("Ingrese el segundo número:"));
+let resultado = num1 + num2;
+console.log("El resultado de la suma es:"+ resultado);// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// Nota: Tener en cuenta la siguiente función: parseInt
+
+let num1 = parseInt(prompt("Ingrese el primer número:"));
+let num2 = parseInt(prompt("Ingrese el segundo número:"));
+let resultado = num1 + num2;
+console.log("El resultado de la suma es:"+ resultado);
