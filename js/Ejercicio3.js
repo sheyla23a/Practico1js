4fe641e13e8d262f06eb844afcd90651ce6cfc9a
sheyla astorga <sheylaastorga1998@gmail.com>
2023-10-26 17:35:42 -0300

    agregue consigna ejercicio 3 de trello y lo desarrolle

---
+++ js/Ejercicio3.js
@@ -0,0 +1,9 @@
+// Conversión de Grados Celsius a Fahrenheit
+
+// Crea una variable llamada gradosCelsius y asígnale un valor en grados Celsius. Luego, utiliza la fórmula de conversión para calcular los grados Fahrenheit y almacénalos en una variable llamada gradosFahrenheit. Muestra ambos valores en la consola.
+
+
+let gradosCelsius = 25; 
+let gradosFahrenheit = (gradosCelsius * 9/5) + 32;
+console.log("Grados Celsius: " + gradosCelsius);
+console.log("Grados Fahrenheit: " + gradosFahrenheit);
